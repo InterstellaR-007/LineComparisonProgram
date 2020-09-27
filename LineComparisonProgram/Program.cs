866315003161ca0e1b9df7ce837a2bc76d49385d
InterstellaR-007 <intrstelr.007@gmail.com>
2020-09-28 01:18:58 +0530

    [Anuj] Line Length UC1

--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -7,9 +7,28 @@ namespace LineComparisonProgram
     {
         static void Main(string[] args)
         {
-                Console.WriteLine("Welcome to Line Comparison Program on Master Branch");
+            int[] x = new int[2];
+            int[] y = new int[2];
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter the X and Y coordinate for Line" + (i + 1));
+                x[i] = Convert.ToInt32(Console.ReadLine());
+                y[i] = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+
+            Double v = LengthOfLine(x[0], y[0], x[1], y[1]);
+            Console.WriteLine("The length of Line is : " + v);
 
         }
-       
+        public static int LengthOfLine(int x1, int y1, int x2, int y2)
+        {
+            int lengthOfLine;
+            lengthOfLine = (int)Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+            ////Math.Pow((x1 - x2), 2) +Math.Pow((y1-y2),2)
+            return lengthOfLine;
+
+        }
     }
 }
