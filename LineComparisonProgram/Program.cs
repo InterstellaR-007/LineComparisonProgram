4279c9a025099d33be1c856d0c17e46876208206
unknown <intrstelr.007@gmail.com>
2020-11-24 20:27:51 +0530

    [Anuj] Updated UC1

--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -3,8 +3,15 @@ using System.Runtime.ConstrainedExecution;
 
 namespace LineComparisonProgram
 {
+    /// <summary>
+    /// Program to showcase Calculation of line lenght based on input coordinates
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             int[] x = new int[2];
@@ -22,11 +29,18 @@ namespace LineComparisonProgram
             Console.WriteLine("The length of Line is : " + v);
 
         }
+        /// <summary>
+        /// Length the of line.
+        /// </summary>
+        /// <param name="x1">The x1.</param>
+        /// <param name="y1">The y1.</param>
+        /// <param name="x2">The x2.</param>
+        /// <param name="y2">The y2.</param>
+        /// <returns></returns>
         public static int LengthOfLine(int x1, int y1, int x2, int y2)
         {
             int lengthOfLine;
             lengthOfLine = (int)Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
-            ////Math.Pow((x1 - x2), 2) +Math.Pow((y1-y2),2)
             return lengthOfLine;
 
         }
